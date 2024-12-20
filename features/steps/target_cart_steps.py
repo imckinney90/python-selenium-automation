8867170c0d45157792fb154f1e03930b4d1ba1cd
Ian M <mckinneyian90@gmail.com>
2024-12-20 10:50:20 -0500

    Page Object Update

--- features/steps/target_cart_steps.py
+++ features/steps/target_cart_steps.py
@@ -9,7 +9,7 @@ COCA_COLA = (By.CSS_SELECTOR,'[aria-label="Add Coca-Cola Soda - 10pk/7.5 fl oz M
 
 @given('Open main page')
 def open_main(context):
-    context.driver.get('https://www.target.com/')
+    context.app.main_page.open_main()
 
 
 @when('Click on Cart Icon')
